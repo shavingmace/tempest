d875da9472760fe9f7925a4a0b87ce45a25bb4b0
shavingmace <pskyd0151@gamil.com>
2022-11-07 18:10:33 +0900

    models.py 간이 구현 완료

--- tempest/models.py
+++ tempest/models.py
@@ -1,21 +1,38 @@
 from django.db import models
+from common.models import TempestUser
 
-# 만들어야 할 모델 
-# 사용자, 날씨, 의류 카테고리, 사용자 기록 
+# 사용자는 common.models.py에 정의됨.
     
+# 날씨
 class Weather(models.Model):
     # 날짜, 지역, API 응답
     date = models.DateTimeField()
-    region =  models.CharField(max_length=10)
+    region =  models.CharField(max_length=6)
     content = models.TextField()
 
+    def __str__(self):
+        return f'{self.id}: \n\t{self.date}\n\t{self.region}\n\t{self.content}'
 
+# 피복 범주별 테이블 
 # top, outer, bottom, onepiece, shoes, headwear
-class clothing_top(models.Model):
+class Clothing_top(models.Model):
     name = models.CharField(max_length=10)
     
-# class UserRecords(models.Model):
-    # 사용자 ID(외래키), 날짜, 해당 기상 데이터(외래키), 
-    # top, outer, bottom, onepiece, shoes, headwear
-#    pass
+class Clothing_outer(models.Model):
+    name = models.CharField(max_length=10)
+
+class Clothing_bottom(models.Model):
+    name = models.CharField(max_length=10)
+        
+        
+# 피복 기록 
+class ClotheRecords(models.Model):
+    user = models.ForeignKey(TempestUser, on_delete=models.CASCADE)
+    weather = models.ForeignKey(Weather, on_delete=models.CASCADE)
+    top = models.ForeignKey(Clothing_top, on_delete=models.CASCADE)
+    outer = models.ForeignKey(Clothing_outer, on_delete=models.CASCADE)
+    bottom =models.ForeignKey(Clothing_bottom, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return f'{self.user.username}\n{self.weather.date}\n\touter:{self.outer}\n\ttop:{self.top}\n\tbottom:{self.bottom}'
     
--- tempest/views.py
+++ tempest/views.py
@@ -12,8 +12,17 @@ def index(req):
     return HttpResponse("안녕 템페스트")
 
 def jsontest(req):
-    j = {'함성': '우하하',
-         '동물': '쥐',
-         "음식": '치즈'}
+    j = {
+         '쥐': {
+            '함성': '우하하',
+            "음식": '치즈',
+            '집': '땅굴'
+            },
+         '소':{
+            '함성': '음하하',
+            '음식':'치즈양파수프',
+            '집':'외양간'
+         }
+         }
     print(f"json test 시작")
     return JsonResponse(j, safe=False, json_dumps_params={'ensure_ascii': False})
