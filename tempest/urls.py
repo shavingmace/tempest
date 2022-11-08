3c1b5e6bfb1f5229b3d91ad736585f184d0d005a
shavingmace <pskyd0151@gamil.com>
2022-11-08 15:46:41 +0900

    urls.py의 url 핸들링으로 특정 날짜의 날씨 api로 불러오기 구현

--- tempest/urls.py
+++ tempest/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('test/', views.test, name='test'),
     path('jsontest/', views.jsontest, name='jsontest'),
+    path('showwthr/<str:date>', views.show_wthr, name='show_wthr')
 ]
--- tempest/views.py
+++ tempest/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.http import JsonResponse
 
+import json
+import requests as reqs
 
 # Create your views here.
 
@@ -26,3 +28,21 @@ def jsontest(req):
          }
     print(f"json test 시작")
     return JsonResponse(j, safe=False, json_dumps_params={'ensure_ascii': False})
+
+
+def show_wthr(req, date):
+    key='jQGK6Krekt13IVsRLG8PI6pQf+jjzIQjGY2KNi4n6iX9y4Rnl3dd6GVesutZTLvmUNjjy4L6oNmJECojps97JA=='
+    url='http://apis.data.go.kr/1360000/AsosDalyInfoService/getWthrDataList'
+    params = {
+            'serviceKey':  key,    # 서비스코드
+            'pageNo': '1', # 페이지번호
+            'numOfRows':'10', # 한페이지 결과수
+            'dataType': 'JSON', # 응답자료형식
+            'dataCd': 'ASOS',  # 자료코드
+            'dateCd': 'DAY',  # 날짜코드
+            'startDt': date,  # 조회 기간 시작일(YYYYMMDD)
+            'endDt': date,  # 조회 기간 종료일(YYYYMMDD) (전일(D-1)까지 제공)
+            'stnIds': '108'  # 종관기상관측 지점 번호 (108 서울)
+            }
+    res = reqs.get(url, params=params, timeout=10)
+    return JsonResponse(res.json(), safe=False, json_dumps_params={'ensure_ascii': False})
